59fd9da1675840d2fe7adea80e6e5a3e70635f4b
Parag Dulam <paragdulam@gmail.com>
2016-10-26 19:53:21 +0530

    Fixed the server side login

--- config.py
+++ config.py
@@ -14,12 +14,14 @@ class ProductionConfig(Config):
 class StagingConfig(Config):
 	DEVELOPMENT = True
 	APP_ID = "2117381198487388"
+	APP_SECRET = "f9cac49b700d7fbac2fbbe48356d589a"
 	DEBUG = True
 	BASE_URL = "https://shoplikes-staging.herokuapp.com"
 	
 class DevelopmentConfig(Config):
 	DEVELOPMENT = True
 	APP_ID = "1485658648363694"
+	APP_SECRET = "ebf9436f2c97491f3f70a59a88d8f595"
 	DEBUG = True
 	BASE_URL = "http://localhost:8080"
 	
