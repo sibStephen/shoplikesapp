fdb5b61c7ad7db7499bb2feef3195e9c40c938c5
Parag Dulam <paragdulam@gmail.com>
2016-11-16 20:10:08 +0530

    Celery Test 7

--- app/__init__.py
+++ app/__init__.py
@@ -547,7 +547,6 @@ def saveUserInfo(user_id, access_token):
 	user = User.query.filter_by(user_id=result['id'])
 	user.first_name = result["first_name"]
 	user.last_name = result["last_name"]
-	db.session.add()
 	db.session.commit()
 
 
