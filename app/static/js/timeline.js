a7671425fcccd694a51b48748d7d4fa8c89cd89a
Parag Dulam <paragdulam@gmail.com>
2016-12-12 22:22:32 +0530

    Removed reco_cnt from User in timeline viewfor now.

--- app/static/js/timeline.js
+++ app/static/js/timeline.js
@@ -223,7 +223,7 @@ function getFriends(friends_url, add_header) {
 
 		for (i in friends) {
 			var friend = friends[i];
-			innerHTML += "<tr onclick=\"friendClicked('"+friend.id+"')\"><td>"+friend.name+"</td></tr>"
+			innerHTML += "<tr onclick=\"friendClicked('"+friend.id+"')\"><td><img src=\"https://graph.facebook.com/"+friend.id+"/picture\" style=\"width:40px;border-radius:20px;height:40px\"></img> "+friend.name+"</td></tr>";
 		}
 
 		tableNode.innerHTML = innerHTML;
